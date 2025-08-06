0fa6d74eb7da550a9834898d2e99c6b3356d00a5
robinjusticeleague <robinjusticeleague@gmail.com>
2025-08-06 13:07:27 +0000

    feat: oxc

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,43 +1,25 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  channel = "stable-24.05";
   packages = [
-    pkgs.nodejs_20
-    pkgs.python3
+    pkgs.rustup
+    pkgs.gcc
+    pkgs.tree
+    pkgs.gnumake
   ];
-  # Sets environment variables in the workspace
-  env = {};
+  env = { };
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "pkief.material-icon-theme"
+      "ziglang.vscode-zig"
+      "tamasfe.even-better-toml"
+      "rust-lang.rust-analyzer"
     ];
-    # Enable previews and customize configuration
-    previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
-          manager = "web";
-        };
-      };
-    };
-    # Workspace lifecycle hooks
     workspace = {
-      # Runs when a workspace is first created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "style.css" "main.js" "index.html" ];
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        install = "rustup default stable && rustup update && cargo run";
+        default.openFiles = [
+          "README.md"
+        ];
       };
     };
   };
